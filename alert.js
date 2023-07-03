ca38fee0c359899022db0e9fa123fb48f716589e
Manas-tech <115521778+Manas-tech@users.noreply.github.com>
2023-07-03 22:16:21 +0530

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,6 +1,6 @@
 /*console.log("WEBSITE");
 window.alert("This is a spam website");*/
-let age=window.prompt("Type conversion enter a number")
+let age=window.prompt("Type conversion  enter a number")
 console.log(typeof age)
 age=Number(age)
 console.log(typeof age)
