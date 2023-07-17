38c0d129bd763fc3e592c91f6ee7ecdf5a944d35
Manas-tech <115521778+Manas-tech@users.noreply.github.com>
2023-07-17 11:29:33 +0530

    Update while.js

--- while.js
+++ while.js
@@ -1,5 +1,5 @@
 let username="";
 while(username == "" || username==null){
-    username=window.prompt("Enter your name");
+    username=window.prompt("Enter your name ");
 }
 console.log("Hello",username)
