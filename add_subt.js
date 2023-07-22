2ab167726a9c2dd8946a634c85bcb44170b3ed24
Manas-tech <115521778+Manas-tech@users.noreply.github.com>
2023-07-22 21:46:00 +0530

    Update add_subt.js

--- add_subt.js
+++ add_subt.js
@@ -5,7 +5,7 @@ function addthreenumbers(a,b,c){
 let c=addthreenumbers(1,2,54)
 console.log(c)
 
-console.log("this program subtracts 3 numbers")
+console.log("this program subtracts 3  numbers")
 function subtthreenumbers(a,b,c){
     return a-b-c;
 }
